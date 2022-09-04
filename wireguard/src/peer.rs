58f39e2d57b0617ed7577d17ab491604bce004bf
noah <nomaxx117@gmail.com>
2022-09-03 21:32:20 -0500

    checkpoint

--- wireguard/src/peer.rs
+++ wireguard/src/peer.rs
@@ -1,15 +1,22 @@
 use crate::wire;
+use blake2::{Blake2s, Digest};
 use chacha20poly1305::{AeadInPlace, ChaCha20Poly1305, Key, KeyInit, Nonce};
+use x25519_dalek::ReusableSecret;
+
+const SESSIONS: usize = 3;
+const CONSTRUCTION: &[u8; 37] = b"Noise_IKpsk2_25519_ChaChaPoly_BLAKE2s";
+const IDENTIFIER: &[u8; 34] = b"WireGuard v1 zx2c4 Jason@zx2c4.com";
 
 pub struct Peer {
     public_key: [u8; 32],
-    sessions: [Option<Box<PeerSession>>; 3],
+    sessions: [Option<Box<PeerSession>>; SESSIONS],
     current: usize,
+    index: u32,
 }
 
 // todo put keys into something that zeroes out
 struct PeerSession {
-    ephemeral_private: [u8; 32],
+    ephemeral_private: ReusableSecret,
     ephemeral_public: [u8; 32],
     psk: [u8; 32],
     hash: [u8; 32],
@@ -19,7 +26,24 @@ struct PeerSession {
     // not needed the way this is currently implemented
     _rx_nonce: u64,
     tx_nonce: u64,
-    index: u32,
+    tx_index: u32,
+}
+
+impl PeerSession {
+    fn new(ephemeral_private: ReusableSecret) -> Self {
+        Self {
+            ephemeral_private,
+            ephemeral_public: [0; 32],
+            psk: [0; 32],
+            hash: [0; 32],
+            chaining: [0; 32],
+            tx_key: [0; 32],
+            rx_key: [0; 32],
+            _rx_nonce: 0,
+            tx_nonce: 0,
+            tx_index: 0,
+        }
+    }
 }
 
 pub struct Capabilities {
@@ -42,8 +66,62 @@ impl Peer {
         todo!()
     }
 
-    pub fn next(&self) -> usize {
-        (self.current + 1) % 3
+    fn initiate_handshake(&mut self, static_public: &[u8; 32], packet: &mut [u8]) {
+        let new_idx = (self.current + 1) % SESSIONS;
+
+        if self.sessions[new_idx].is_none() {
+            self.sessions[new_idx] = Some(Box::new(PeerSession::new(dh_generate())));
+        }
+
+        let session = self.sessions[new_idx].as_mut().unwrap();
+
+        hash(&[CONSTRUCTION], &mut session.chaining);
+
+        let mut temp = [0; 32];
+        hash(&[&session.chaining, IDENTIFIER], &mut temp);
+        hash(&[&temp, &self.public_key], &mut session.hash);
+
+        let mut msg = wire::HandshakeInitiation::new(packet);
+
+        *msg.ty_mut() = 0x01;
+        *msg.reserved_mut() = [0; 3];
+        *msg.sender_mut() = self.index.to_le_bytes();
+    }
+
+    fn encode(&mut self, headers: &mut [u8], packet: &mut Vec<u8>) {
+        let current_session = self.sessions[self.current].as_mut().unwrap();
+
+        // apply padding to packet to make this a multiple of 16
+        // this would be simpler if div_ceil were stable!!!
+        let padding_amount = 16 * ((packet.len() + 16 - 1) / 16) - packet.len();
+        packet.resize(packet.len() + padding_amount, 0);
+
+        // encrypt
+        seal_aead(
+            &current_session.tx_key,
+            current_session.tx_nonce,
+            packet,
+            &[],
+        );
+
+        // set header fields
+        let mut msg_headers = wire::TransportData::new(headers);
+        *msg_headers.ty_mut() = 0x04;
+        *msg_headers.receiver_mut() = current_session.tx_index.to_le_bytes();
+        *msg_headers.counter_mut() = current_session.tx_nonce.to_le_bytes();
+
+        // advance the nonce counter
+        current_session.tx_nonce += 1;
+    }
+
+    fn decode(&mut self, headers: &[u8], packet: &mut Vec<u8>) -> bool {
+        let current_session = self.sessions[self.current].as_mut().unwrap();
+
+        let msg_headers = wire::TransportData::new(headers);
+
+        let nonce = u64::from_le_bytes(*msg_headers.counter());
+
+        unseal_aead(&current_session.rx_key, nonce, packet, &[])
     }
 }
 
@@ -83,39 +161,11 @@ impl Capabilities {
 
 impl EncodeDecodeCapability {
     pub fn encode(&self, peer: &mut Peer, headers: &mut [u8], packet: &mut Vec<u8>) {
-        let current_session = peer.sessions[peer.current].as_mut().unwrap();
-
-        // apply padding to packet to make this a multiple of 16
-        // this would be simpler if div_ceil were stable!!!
-        let padding_amount = 16 * ((packet.len() + 16 - 1) / 16) - packet.len();
-        packet.resize(packet.len() + padding_amount, 0);
-
-        // encrypt
-        seal_aead(
-            &current_session.tx_key,
-            current_session.tx_nonce,
-            packet,
-            &[],
-        );
-
-        // set header fields
-        let mut msg_headers = wire::TransportData::new(headers);
-        *msg_headers.ty_mut() = 0x04;
-        *msg_headers.receiver_mut() = current_session.index.to_le_bytes();
-        *msg_headers.counter_mut() = current_session.tx_nonce.to_le_bytes();
-
-        // advance the nonce counter
-        current_session.tx_nonce += 1;
+        peer.encode(headers, packet)
     }
 
     pub fn decode(&self, peer: &mut Peer, headers: &[u8], packet: &mut Vec<u8>) -> bool {
-        let current_session = peer.sessions[peer.current].as_mut().unwrap();
-
-        let msg_headers = wire::TransportData::new(headers);
-
-        let nonce = u64::from_le_bytes(*msg_headers.counter());
-
-        unseal_aead(&current_session.rx_key, nonce, packet, &[])
+        peer.decode(headers, packet)
     }
 }
 
@@ -135,6 +185,22 @@ impl ReceiveHandshakeResponseCapability {
     pub fn consume(self, _peer: &mut Peer, _packet: &mut [u8]) {
         todo!()
     }
+}
+
+fn dh_generate() -> ReusableSecret {
+    x25519_dalek::ReusableSecret::new(rand_core::OsRng)
+}
+
+fn hash(input: &[&[u8]], output: &mut [u8]) {
+    let mut hasher: Blake2s<blake2::digest::consts::U32> = Blake2s::new();
+
+    for slice in input {
+        hasher.update(slice);
+    }
+
+    hasher.finalize_into(blake2::digest::generic_array::GenericArray::from_mut_slice(
+        output,
+    ));
 }
 
 fn unseal_aead(key: &[u8; 32], counter: u64, plaintext: &mut Vec<u8>, auth: &[u8]) -> bool {
